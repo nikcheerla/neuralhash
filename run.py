e85a8347a19687b6cf7ab1987f31b2c9dc36fe9c
Nikhil Cheerla <nikcheerla@gmail.com>
2018-05-29 23:49:47 -0700

    jobs api finished

--- run.py
+++ run.py
@@ -1,20 +1,24 @@
 
-import random, sys, os, glob
-import argparse, subprocess
+import random, sys, os, glob, yaml
+import argparse, subprocess, shutil
 
-parser = argparse.ArgumentParser(description='Runs arbitary jobs with experiment tagging and output saving.')
+parser = argparse.ArgumentParser(description='Runs arbitary jobs with experiment configging and output saving.')
 
 parser.add_argument("cmd")
 parser.add_argument('--type', default="experiment", help='type of run')
-parser.add_argument('--tag', default="experiment", help='tag for the experiment')
+parser.add_argument('--config', default="experiment", help='configuration tag for the experiment')
 args = parser.parse_args()
 
-run_log = yaml.load(open(".runlog.yml"))
+try:
+	run_log = yaml.load(open("jobs/runlog.yml"))
+except:
+	run_log = {}
+
 run_data = run_log[args.type] = run_log.get(args.type, {})
 run_data["runs"] = run_data.get("runs", 0) + 1
 run_name = args.type + str(run_data["runs"])
 
-run_data[run_name] = {"tag": args.tag, "status": "In Progress"}
+run_data[run_name] = {"config": args.config, "cmd": args.cmd, "status": "In Progress"}
 
 process = subprocess.Popen(args.cmd, shell=True)
 
@@ -25,22 +29,25 @@ def monitor_process(process, run_data):
 			pass
 		except (KeyboardInterrupt, SystemExit):
 			# Program shut down
-			run_data[run_name] = {"tag": args.tag, "status": "Shutdown"}
+			run_data[run_name] = {"config": args.config, "status": "Shutdown"}
+			process.kill()
 			return
 
-	try:
-	 	subprocess.check_call(command)
-		comment = input("Add a comment describing results? [ENTER to skip]: ")
-		run_data[run_name] = {"tag": args.tag, "status": "Complete", "comment": comment}
-
-	except subprocess.CalledProcessError:
-		run_data[run_name] = {"tag": args.tag, "status": "Error"}
+	if process.poll() == 0:
+		result = input ("Add a comment describing results? [ENTER to skip]: ")
+		run_data[run_name] = {"config": args.config, "cmd": args.cmd, \
+							"status": "Complete", "results": result}
+	else:
+		run_data[run_name] = {"config": args.config, "cmd": args.cmd, "status": "Error"}
 
 monitor_process(process, run_data)
-yaml.dump(run_log, ".runlog.yml")
 
-output_directory = "outputs/"
-yaml.dump(run_log, ".yml")
+shutil.copytree("output", f"jobs/{run_name}/output")
+yaml.safe_dump(run_log, open("jobs/runlog.yml", 'w'), \
+		allow_unicode=True, default_flow_style=False)
+yaml.safe_dump(run_data[run_name], open(f"jobs/{run_name}/comments.yml", 'w'), \
+		allow_unicode=True, default_flow_style=False)
+
 
 
 
