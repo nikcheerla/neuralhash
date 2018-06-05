b82b7ebc27b3dd17a43e4646f23053bf28fd23d4
Nikhil Cheerla <nikcheerla@gmail.com>
2018-06-05 02:14:01 -0700

    better train

--- train.py
+++ train.py
@@ -25,70 +25,61 @@ from testing import test_transforms
 
 
 
-logger = Logger("train", ("bce", "bits"))
+logger = Logger("train", ("loss", "bits"), print_every=4, plot_every=40)
 
-def p(x):
-    x = transforms.resize_rect(x)
-    x = transforms.rotate(transforms.scale(x, 0.6, 1.4), max_angle=30)
-    x = transforms.gauss(x, min_sigma=0.8, max_sigma=1.2)
-    x = transforms.translate(x)
-    x = transforms.identity(x)
-    return x
+def loss_func(model, x, targets):
+	scores = model.forward(x)
+	predictions = scores.mean(dim=1)
+	score_targets = binary.target(targets).unsqueeze(1).expand_as(scores)
 
-# returns the loss for the image
-def loss_func(model, x):
-	scores = model.forward(x, distribution=p, n=BATCH_SIZE, return_variance=False) # (N, T)
-	predictions = scores.mean(dim=0)
-	logger = 
-	return F.binary_cross_entropy(scores, binary.target(target).expand(BATCH_SIZE, TARGET_SIZE)), \
+	return F.binary_cross_entropy(scores, score_targets), \
 		predictions.cpu().data.numpy().round(2)
-	# return F.mse_loss(predictions, binary.target(target).repeat(BATCH_SIZE, 1)), \
-	#             predictions.mean(dim=0).cpu().data.numpy().round(2)
-
-def loss_func(model, encoded_im, target):
-	
-	scores = model.forward(encoded_im, distribution=p, n=64) # (N, T)
-	predictions = scores.mean(dim=0)
-	bce_loss = F.binary_cross_entropy(scores, \
-		binary.target(target).repeat(64, 1))
-
-	logger.step("bce", bce_loss)
-
-	return bce_loss, predictions.cpu().data.numpy().round(2)
 
 def create_targets():
 	return [(i == j) for i, j in product(range(TARGET_SIZE), range(TARGET_SIZE))]
 
 if __name__ == "__main__":
 
-	model = DecodingNet()
+	def p(x):
+	    x = transforms.resize_rect(x)
+	    x = transforms.rotate(transforms.scale(x, 0.6, 1.4), max_angle=30)
+	    x = transforms.gauss(x, min_sigma=0.8, max_sigma=1.2)
+	    x = transforms.translate(x)
+	    x = transforms.identity(x)
+	    return x
+
+	model = nn.DataParallel(DecodingNet(n=48, distribution=p))
+	optimizer = torch.optim.Adadelta(model.module.classifier.parameters(), lr=1.5e-2)
 	model.train()
-	optimizer = torch.optim.Adadelta(model.classifier.parameters(), lr=1.5e-2)
-	
+		
 	def data_generator():
 
 		files = glob.glob("data/colornet/*.jpg")
 		while True:
 			img = im.load(random.choice(files))
 			if img is None: continue
+			target = binary.random(TARGET_SIZE)
 			yield img, target	
 
 	def checkpoint():
 		print (f"Saving model to {OUTPUT_DIR}train_test.pth")
-		model.save(OUTPUT_DIR + "train_test.pth")
+		model.module.save(OUTPUT_DIR + "train_test.pth")
 
 	logger.add_hook(checkpoint)
 
-	for images, targets in batched(data_generator()):
+	for i, (images, targets) in enumerate(batched(data_generator(), batch_size=48)):
 		images = im.stack(images)
-		encoded_images = encode_binary(images, model, target, verbose=False, max_iter=4)
-		loss, predictions = loss_func(model, encoded_images, target)
+
+		encoded_images = encode_binary(images, targets, model, verbose=False, max_iter=3)
+		loss, predictions = loss_func(model, encoded_images, targets)
+		logger.step ("loss", loss)
 
 		optimizer.zero_grad()
 		loss.backward()
 		optimizer.step() 
 
-		logger.step ("bits", binary.distance(predictions, target))
+		error = np.mean([binary.distance(x, y) for x, y in zip(predictions, targets)])
+		logger.step ("bits", error)
 
 		if (i+1) % 400 == 0:
 			test_transforms(model)
