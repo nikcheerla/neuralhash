661c64f7d79a7beebb1a9ec8599f58ba6b2540b6
Nikhil Cheerla <nikcheerla@gmail.com>
2018-07-19 10:54:41 -0700

    amnesia

--- train_amnesia.py
+++ train_amnesia.py
@@ -54,7 +54,7 @@ def init_data(input_path, output_path, n=100):
 
 if __name__ == "__main__":	
 
-	model = nn.DataParallel(DecodingNet(n=64, distribution=transforms.encoding))
+	model = nn.DataParallel(DecodingNet(n=48, distribution=transforms.encoding))
 	# params = itertools.chain(model.module.classifier.parameters(), 
 	# 						model.module.features[-1].parameters())
 	optimizer = torch.optim.Adam(model.module.classifier.parameters(), lr=2.5e-3)
@@ -87,6 +87,6 @@ if __name__ == "__main__":
 		error = np.mean([binary.distance(x, y) for x, y in zip(predictions, targets)])
 		logger.step ("bits", error)
 
-		if i == 0 and i % 100 == 0:
+		if i != 0 and i % 100 == 0:
 			test_transforms(model, name=f'iter{i}')
 
